1b1c80fbc268b627f3c285d46ea3bf14185057ba
Ramsundhar Venkadesan <42778998+ramsundharVenkadesan@users.noreply.github.com>
2026-04-01 12:41:43 -0400

    Delete CliniGraph/CliniGraph directory

--- CliniGraph/CliniGraph/Agent/Reflection/State.py
+++
@@ -1,7 +0,0 @@
-from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
-from langgraph.graph import add_messages
-
-
-class MessageGraph(TypedDict):
-    messages:Annotated[list, add_messages]
