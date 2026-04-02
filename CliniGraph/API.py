26e431a7f44a4f04f64bd17dcfd137f7e1d126b8
nealgoogs <ngeoghegan@my.ccsu.edu>
2026-04-02 11:33:21 -0400

    Database to object storage

--- CliniGraph/API.py
+++ CliniGraph/API.py
@@ -1,16 +1,22 @@
+import os
 from fastapi import FastAPI, Request
 from starlette.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from dotenv import load_dotenv
 load_dotenv()
-import RAG, REACT
-clini_clarity = FastAPI()
 
+import Feedback
+clini_clarity = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-clini_clarity.include_router(router=RAG.rag_router)
-clini_clarity.include_router(router=REACT.react_router)
+# Only load RAG/REACT routers if API keys are configured
+if os.environ.get("GOOGLE_API_KEY"):
+    import RAG, REACT
+    clini_clarity.include_router(router=RAG.rag_router)
+    clini_clarity.include_router(router=REACT.react_router)
+
+clini_clarity.include_router(router=Feedback.feedback_router)
 
 @clini_clarity.get("/", response_class=HTMLResponse)
 async def index(request: Request):
