3984e8aa9c4b8fe100e3862b6be17c89701037a5
Ramsundhar Venkadesan <42778998+ramsundharVenkadesan@users.noreply.github.com>
2026-04-19 12:37:54 -0400

    Delete CliniGraph directory

--- CliniGraph/Agent/Reflection/State.py
+++
@@ -1,6 +0,0 @@
-from typing import TypedDict, Annotated # Import Type-Hints package
-from langgraph.graph import add_messages # Import function to properly append messages to the state dictionary
-
-
-class MessageGraph(TypedDict): # State dictionary to maintain state between nodes
-    messages:Annotated[list, add_messages] # A list of messages that will be maintained
